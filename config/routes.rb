b67058225880eade05f88be7b2ce855ab96d9892
Rails Udemy Course <railsudemycourse@gmail.com>
2016-07-15 02:27:31 +0000

    edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,11 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
+  resources :users
 
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
-  
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
